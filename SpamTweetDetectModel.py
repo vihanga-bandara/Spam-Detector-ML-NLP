43d2df72fc03d698bd1f1897e3b097525f5523c5
vihanga-bandara <vihanga123@gmail.com>
2019-03-13 22:42:00 +0530

    Code Refactor and added Twitter API support

--- SpamTweetDetectModel.py
+++ SpamTweetDetectModel.py
@@ -21,4 +21,24 @@ print('Numpy: {}', format(numpy.__version__))
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('')
+# load the dataset   
+df = pd.read_csv('SpamTweetsFinalDataset.csv')
+
+# print general information about the dataset that is loaded
+print(df.info())
+print(df.head())
+
+# check the class balance ratio / distribution
+columnNames = list(df.head(0))
+classes = df[columnNames[1]]
+print(classes.value_counts())
+
+# pre-processing the data before classification
+
+# convert the labels into binary values 
+# where 0 = ham and 1 = spam
+from sklearn.preprocessing import OneHotEncoder
+
+binaryEncoder = OneHotEncoder()
+
+DatasetY = classes.apply(binaryEncoder.fit_transform)
