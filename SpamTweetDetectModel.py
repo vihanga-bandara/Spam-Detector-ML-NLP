2a899c0537bffa53ba2fbc1a20d76b7916c21bf5
vihanga-bandara <vihanga123@gmail.com>
2019-03-14 14:38:11 +0530

    Tweet Prediction Model Initial Classification development

--- SpamTweetDetectModel.py
+++ SpamTweetDetectModel.py
@@ -23,7 +23,7 @@ import numpy as np
 import nltk
 
 # load the dataset   
-df = pd.read_csv('SpamTweetsFinalDataset.csv')
+df = pd.read_csv('SpamTweetsFinalDataset.csv', header=None)
 
 # print general information about the dataset that is loaded
 print(df.info())
@@ -41,14 +41,15 @@ print(classes.value_counts())
 from sklearn.preprocessing import LabelEncoder
 labelEncoder = LabelEncoder()
 Y = labelEncoder.fit_transform(classes)
-
+print(classes[:10])
+print(Y[:10])
 # convert the class labels using onehotencoder
 # from sklearn.preprocessing import OneHotEncoder
 # binaryEncoder = OneHotEncoder()
 # X = binaryEncoder.fit_transform(classes).toarray()
 
 # store the twitter data
-tweets = df['Tweet'].str.strip()
+tweets = df[0].str.strip()
 
 # using regex to identify different combinations in the tweet
 
@@ -109,7 +110,7 @@ print('Number of words: {}'.format(len(all_words)))
 print('Most Common words: {}'.format(all_words.most_common(500)))
 
 # use the 1500 most common words as features
-word_features = list(all_words.keys())[:500]
+word_features = list(all_words.keys())[:50]
 
 
 #define a find features function
@@ -122,23 +123,160 @@ def find_features(tweet):
     return features
 
 # example
-# features = find_features(processed['Tweet'])
-# for key,value in features.items():
-#    if value == True:
-#        print (key)
+features = find_features(processed[65])
+for key, value in features.items():
+    if value == True:
+        print(key)
 
 # find features for all tweets
 tweets = zip(processed, Y)
+tweetsList = list(tweets)
 
 # define a seed for reproducibility
 seed = 1
 np.random.seed = seed
-np.random.shuffle(tweets)
+np.random.shuffle(tweetsList)
 
 # call find features function for each tweet
-featuresets = [(find_features(text), label) for (text, label) in tweets]
+featuresets = [(find_features(text), label) for (text, label) in tweetsList]
 
 # split training and testing data sets using sklearn
 from sklearn import model_selection
 
 training, testing = model_selection.train_test_split(featuresets, test_size=0.2, random_state=seed)
+print('Training: {}'.format(len(training)))
+print('Testing: {}'.format(len(testing)))
+
+# classification training using scikit-learn
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors.classification import KNeighborsClassifier
+from sklearn.ensemble.forest import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model.stochastic_gradient import SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+
+# classifaction tools and matrix
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+# from sklearn.svm.classes import OneClassSVM
+# from sklearn.ensemble.voting_classifier import VotingClassifier
+# from sklearn.ensemble.weight_boosting import AdaBoostClassifier
+# from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
+# from sklearn.ensemble.bagging import BaggingClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.svm import LinearSVC
+# from sklearn.mixture import GaussianMixture
+
+# wrap classification model  in NLTK
+from nltk.classify.scikitlearn import SklearnClassifier
+
+# DecisionTreeClassifier 
+nltk_model_decision_tree = SklearnClassifier(DecisionTreeClassifier())
+nltk_model_decision_tree.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_model_decision_tree, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_model_decision_tree, testing) * 100
+
+print('## DecisionTree Classifier ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# KNeighborsClassifier 
+nltk_model_decision_tree = SklearnClassifier(KNeighborsClassifier())
+nltk_model_decision_tree.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_model_decision_tree, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_model_decision_tree, testing) * 100
+
+print('## KNeighborsClassifier ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# RandomForestClassifier 
+nltk_model_decision_tree = SklearnClassifier(RandomForestClassifier())
+nltk_model_decision_tree.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_model_decision_tree, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_model_decision_tree, testing) * 100
+
+print('## RandomForestClassifier ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# LogisticRegression 
+nltk_model_decision_tree = SklearnClassifier(LogisticRegression())
+nltk_model_decision_tree.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_model_decision_tree, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_model_decision_tree, testing) * 100
+
+print('## LogisticRegression ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# SVM Linear 
+nltk_model_decision_tree = SklearnClassifier(SVC())
+nltk_model_decision_tree.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_model_decision_tree, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_model_decision_tree, testing) * 100
+
+print('## SVM Linear ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# NaiveBayes 
+nltk_model_decision_tree = SklearnClassifier(MultinomialNB())
+nltk_model_decision_tree.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_model_decision_tree, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_model_decision_tree, testing) * 100
+
+print('## NaiveBayes ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# SGDClassifier 
+nltk_model_decision_tree = SklearnClassifier(SGDClassifier())
+nltk_model_decision_tree.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_model_decision_tree, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_model_decision_tree, testing) * 100
+
+print('## SGDClassifier ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# ensemble method - Voting Classifier
+from sklearn.ensemble import VotingClassifier
+
+# Defining the model that will be trained
+names = ['Decision Tree', 'Random Forest', 'Logistic Regression', 'SGD Classifier']
+
+classifier = [DecisionTreeClassifier(), RandomForestClassifier(), LogisticRegression(), SGDClassifier()]
+
+models = zip(names, classifier)
+models_list = list(models)
+
+nltk_ensemble = SklearnClassifier(VotingClassifier(estimators=models_list, voting='hard', n_jobs=1))
+nltk_ensemble.train(training)
+accuracy_training = nltk.classify.accuracy(nltk_ensemble, training) * 100
+accuracy_testing = nltk.classify.accuracy(nltk_ensemble, testing) * 100
+
+print('## Ensemble Method Voting Classifier Accuracy ##')
+print('Accuracy_Training: {}'.format(accuracy_training))
+print('Accuracy_Testing: {}'.format(accuracy_testing))
+print('####')
+
+# making class label prediction for testing set
+text_features, labels = zip(*testing)
+
+prediction = nltk_ensemble.classify_many(text_features)
+
+# print a classification report and a confusion matrix
+print(classification_report(labels, prediction))
+
+pd.DataFrame(
+    confusion_matrix(labels, prediction),
+    index=[['actual', 'actual'], ['ham', 'spam']],
+    columns=[['predicted', 'predicted'], ['ham', 'spam']])
