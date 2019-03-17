81ba59a367d5984ce81c4699789ab7c1b76f30b9
vihanga-bandara <vihanga123@gmail.com>
2019-03-18 03:50:40 +0530

    Fetch Random Tweet and Initial Development of SpamDetector

--- SpamDetector.py
+++ SpamDetector.py
@@ -1,4 +1,11 @@
+import pandas as pd
+import numpy as np
+import nltk
 import pickle
+import TwitterAuthentication
+from nltk.tokenize import word_tokenize
+
+
 
 # load the SpamTweetDetectModel word_features from directory
 filename = "wordfeatures.p"
@@ -7,3 +14,78 @@ word_features = pickle.load(open(filename, 'rb'))
 # load the SpamTweetDetectModel from directory
 filename = "SpamTweetDetectModel.sav"
 nltk_ensemble = pickle.load(open(filename, 'rb'))
+
+
+# pre-process incoming data
+
+def preprocess(tweet):
+    # using regex to identify different combinations in the tweet
+
+    # replacing email addresses with 'emailaddr'
+    processed = tweet.replace(r'^.+@[^\.].*\.[a-z]{2,}$', 'emailaddr')
+
+    # replacing links / web addresses with 'webaddr'
+    processed = processed.replace(
+        r'(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?', 'webaddr')
+
+    # replacing money symbol with 'moneysymb'
+    processed = processed.replace(r'$', 'moneysymbol')
+
+    # replacing normal numbers with numbers
+    # some not working might need to manually remove
+    processed = processed.replace(r'^(\+|-)?\d+$', 'numbr')
+
+    # remove punctuation
+    processed = processed.replace(r'[^\w\d\s]', ' ')
+
+    # replaces whitespaces between terms with single space
+    processed = processed.replace(r'\s+', ' ')
+
+    # removing leading and trailing whitespaces
+    processed = processed.replace(r'^s+|\s+?$', '')
+
+    # try hashtagsss as well which can be a new feature
+
+    # change all letters to lowercase
+    processed = processed.lower()
+
+    # remove stop words or useless meaningless words from the tweets
+
+    # from nltk.corpus import stopwords
+    #
+    # stop_words = set(stopwords.words('english'))
+
+    # processed = processed.apply(lambda x: ' '.join(term for term in x.split() if term not in stop_words))
+    #
+    # # using a Porter stemmer to remove word stems
+    # ps = nltk.PorterStemmer()
+    # processed = processed.apply(lambda x: ' '.join(ps.stem(term) for term in x.split()))
+    return processed
+
+
+# define a find features function
+def find_features(tweet):
+    words = word_tokenize(tweet)
+    features = {}
+    for word in word_features:
+        features[word] = (word in words)
+
+    return features
+
+
+# retrieve random tweet
+# for now using a known spam account to retrieve tweet
+
+# create and initialise Twitter API object
+TwitterAPI = TwitterAuthentication.TwitterAuthentication()
+
+# authenitcate the twitter object before using API
+TwitterAPI.authenticate()
+
+# retrieve random tweet - know spam tweet for now
+tweet = TwitterAPI.getRandomTweet("EmptyForNow")
+
+tweet = preprocess(tweet.text)
+
+features_test = find_features(tweet)
+prediction_test = nltk_ensemble.classify(features_test)
--- TwitterAuthentication.py
+++ TwitterAuthentication.py
@@ -7,6 +7,7 @@ class TwitterAuthentication:
     _consumer_key = 'VhzN7FMeTq7lKvwAKV02Ho9Dw'
     _consumer_secret = 'K8pAy6MTLtMB5R5WSklrR0lqudb75zFkjfHWtscHqj7YYixsRr'
     _auth = None
+    _api = None
 
     def __init__(self):
         self._auth = tweepy.OAuthHandler(self._consumer_key, self._consumer_secret)
@@ -14,4 +15,17 @@ class TwitterAuthentication:
 
     def authenticate(self):
         api = tweepy.API(self._auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+        self._api = api
         return api
+
+    def getRandomTweet(self, tweetId):
+        tweetId = 1107192300692660224
+        tweet = self._api.get_status(id=tweetId)
+        print(tweet.text)
+        return tweet
+
+    def getUser(self, tweetUserId):
+        tweetUserId = 'nottoraretaaka'
+        tweetUser = self._api.get_user(tweetUserId)
+        print(tweetUser)
+        return tweetUser
