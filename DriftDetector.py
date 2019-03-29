41b207c2f5fd5dc42095a543b78a746ccc0d12f8
vihanga-bandara <vihanga123@gmail.com>
2019-03-30 03:01:24 +0530

    Algorithm for retraining using Drift Detector

--- DriftDetector.py
+++ DriftDetector.py
@@ -12,15 +12,47 @@ twitter = TwitterAPI.TwitterAPI()
 preprocessor = preprocessing.preprocessing()
 # authenitcate the twitter object before using API
 twitter.authenticate()
-
 # retrieve random tweet - know spam tweet for now
 tweetObj = twitter.getTweet("EmptyForNow")
-
 tweet = preprocessor.preprocess(tweetObj.text)
+# tokenize the words using the best tokenizer available
+# get each token and add it to a list 
 
-features_test = find_features(tweet)
-prediction_test = nltk_ensemble.classify(features_test)
-print(prediction_test)
+# get the spam only dataset and tokenize or use tfidf or fast text and get the most important words
+# tokenize those words and add it to a list
+
+# two functions are needed here. 
+# two functions are made that would take the retreived random tweet tokens[random_tweet_tokens] and tokenized spam words [spam_tokens] with higher weights
+
+# function1  
+# It will call the vocab API and get for each [random_tweet_tokens] all similiar words 
+# for each similiar word it will compare with each [spam_token] 
+# if one or more comes up positive it will add a counter and then we can decide to end it or not
+# then for each token that has a similarity will be noted and then it will be divided with the total number of words in the [random_tweet_tokens]
+# if this score is better than 0.2 we will consider it as spam 
+
+# function2
+# if the above method does not prove to be useful then it will come to this method
+# it will the vocab API and get for each [spam_token] all similiar words
+# for each similiar word it will compare with each [random_tweet_token]
+# if one or more comes up positive it will add a counter and then we can decide to end it or not
+# we will compare the spam token and the word further and if it is the same
+# it will be taken as a spam tweet
+
+# both functions will finally send out if the tweet is spam or not. maybe a bool
+# if this doesnt work out. It will try to use our unsupervised model to check whether it is in the correct cluster. 
+
+# else the tweet will be shown and it will have the ability to be reported manually and then labelled accordingly by the admin
+
+# if it does work out and if the tweet is recognised as spam using either function 1 or function 2 or unsupervised model
+# we will add that tweet to our dataset and retrain the model.
+
+# manually reported tweets will have the ability to be checked by user and then manually labelled. When the detector accuracy reduces by 80%
+# it will take all the reported tweets that are labelled spam and then add it to dataset and retrained
+
+# have an option where you could give tweets randomly and allow users to label them. If it contains tweets which arent really spam now.
+# if many people report it as not spam. Those data will be removed from the dataset to make the retraining faster and efficient
+# and make the model accuracy higher and precise
 
 # retrieve spam user account detail by using tweet handle
 # for now using a known spam account to retrieve the data
