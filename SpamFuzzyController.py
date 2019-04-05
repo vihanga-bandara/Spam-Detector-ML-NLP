e286c8a64b752d22b298a9561ca43420310f8189
vihanga-bandara <vihanga123@gmail.com>
2019-04-05 12:32:18 +0530

    Trying to apply fuzzy logic to probabilistic values of two classifiers

--- SpamFuzzyController.py
+++ SpamFuzzyController.py
@@ -3,17 +3,25 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
 # New Antecedent/Consequent objects hold universe variables and membership
-# functions
+# Creating two fuzzy input variables and one output fuzzy variable
 tweet_model = ctrl.Antecedent(np.arange(0, 101, 1), 'Tweet Model')
 user_model = ctrl.Antecedent(np.arange(0, 101, 1), 'User Model')
 predict_spam = ctrl.Consequent(np.arange(0, 100, 1), 'Spam Prediction')
 
-# Auto-membership function population is possible with .automf(3, 5, or 7)
-quality.automf(3)
-service.automf(3)
+# # Auto-membership function population is possible with .automf(3, 5, or 7)
+# quality.automf(3)
+# service.automf(3)
+
+# using the automf to populate the fuzzy variables with terms
+tweet_spam_names = ['not_spam', 'maybe_spam', 'definite_spam']
+tweet_user_spam_names = ['not_spam', 'maybe_spam', 'definite_spam']
+model_output_names = ['not_spam', 'spam']
+
+tweet_model.automf(names=tweet_spam_names)
+user_model.automf(names=tweet_user_spam_names)
+user_model.automf(names=model_output_names)
 
 # Custom membership functions can be built interactively with a familiar,
-# Pythonic API
 tip['low'] = fuzz.trimf(tip.universe, [0, 0, 13])
 tip['medium'] = fuzz.trimf(tip.universe, [0, 13, 25])
 tip['high'] = fuzz.trimf(tip.universe, [13, 25, 25])
